8185b47be243e0b8ef862e746b22998922800a19
test <test@HDCS9-D-5.local>
2017-07-12 13:38:21 +0530

    abcd

---
+++ paractice/src/SquareRootTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class SquareRootTest extends TestCase {
+
+
+	@Test
+	public void testSquareRoot() {
+
+	}
+
+	@Test
+	public void testSquareRoot1() {
+
+	}
+
+}
